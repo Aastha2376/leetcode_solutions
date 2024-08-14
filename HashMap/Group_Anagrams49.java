ee69b59614cde437a799ff906c144ce37381028d
Aastha2376 <aastha.arora.btech2022@sitpune.edu.in>
2024-08-14 21:34:33 +0530

    Group_Anagrams

---
+++ HashMap/Group_Anagrams49.java
@@ -0,0 +1,29 @@
+package leetcode_solutions.HashMap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Group_Anagrams49 {
+    class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
+}
